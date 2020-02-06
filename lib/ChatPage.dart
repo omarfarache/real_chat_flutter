1f84ecf380d09b28d97aa1619eb9942787ccce79
Omar <omarfarache91@gmail.com>
2020-02-06 10:52:38 +0200

    Initial commit

---
+++ lib/ChatPage.dart
@@ -0,0 +1,148 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_socket_io/flutter_socket_io.dart';
+import 'package:flutter_socket_io/socket_io_manager.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  SocketIO socketIO;
+  List<String> messages;
+  double height, width;
+  TextEditingController textController;
+  ScrollController scrollController;
+
+  @override
+  void initState() {
+    messages = List<String>();
+
+    textController = TextEditingController();
+    scrollController = ScrollController();
+
+    socketIO = SocketIOManager().createSocketIO(
+      'https://real-time-chat-flutter1.herokuapp.com/', // Here You should put your huroku link
+      '/',
+    );
+
+    socketIO.init();
+
+    socketIO.subscribe('receive_message', (jsonData) {
+      //Convert the JSON data received into a Map
+      Map<String, dynamic> data = json.decode(jsonData);
+      this.setState(() => messages.add(data['message']));
+      scrollController.animateTo(
+        scrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 600),
+        curve: Curves.ease,
+      );
+    });
+
+    socketIO.connect();
+    super.initState();
+  }
+
+  Widget buildSingleMessage(int index) {
+    return Container(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        margin: const EdgeInsets.only(bottom: 20.0, left: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Text(
+          messages[index],
+          style: TextStyle(color: Colors.white, fontSize: 15.0),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMessageList() {
+    return Container(
+      height: height * 0.8,
+      width: width,
+      child: ListView.builder(
+        controller: scrollController,
+        itemCount: messages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return buildSingleMessage(index);
+        },
+      ),
+    );
+  }
+
+  Widget buildChatInput() {
+    return Container(
+      width: width * 0.7,
+      padding: const EdgeInsets.all(2.0),
+      margin: const EdgeInsets.only(left: 40.0),
+      child: TextField(
+        decoration: InputDecoration.collapsed(
+          hintText: 'Send a message...',
+        ),
+        controller: textController,
+      ),
+    );
+  }
+
+  Widget buildSendButton() {
+    return FloatingActionButton(
+      backgroundColor: Colors.blueAccent,
+      onPressed: () {
+        if (textController.text.isNotEmpty) {
+          socketIO.sendMessage(
+              'send_message', json.encode({'message': textController.text}));
+
+          this.setState(() => messages.add(textController.text));
+          textController.text = '';
+
+          scrollController.animateTo(
+            scrollController.position.maxScrollExtent,
+            duration: Duration(milliseconds: 600),
+            curve: Curves.ease,
+          );
+        }
+      },
+      child: Icon(
+        Icons.send,
+        size: 30,
+      ),
+    );
+  }
+
+  Widget buildInputArea() {
+    return Container(
+      height: height * 0.1,
+      width: width,
+      child: Row(
+        children: <Widget>[
+          buildChatInput(),
+          buildSendButton(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: height * 0.1),
+            buildMessageList(),
+            buildInputArea(),
+          ],
+        ),
+      ),
+    );
+  }
+}
